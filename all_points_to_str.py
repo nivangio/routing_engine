f47e6e043619551236839661725ec74de68429ac
Hernan Resnizky <hernan.resnizky@gmail.com>
2020-08-11 02:47:16 -0300

    cast numbers to str

--- all_points_to_str.py
+++ all_points_to_str.py
@@ -3,10 +3,10 @@ def all_points_to_str(config, to_str= True):
     ##Build points and make request
     all_points = []
 
-    all_points.append(config["starting_location"]["long"] + "," + config["starting_location"]["lat"])
+    all_points.append(str(config["starting_location"]["long"]) + "," + str(config["starting_location"]["lat"]))
 
     for i in config["directions"]:
-        all_points.append(i["long"] + "," + i["lat"])
+        all_points.append(str(i["long"]) + "," + str(i["lat"]))
 
     if to_str:
         points_str = ";".join(all_points)
